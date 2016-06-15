81af71198bc45ba22ff11fba6a9d920832bc33a0
Otto Lau <otto.lau@hotmail.com>
2016-06-15 01:18:25 -0700

    Implement aggregation queries

--- CPSC304Project/pages/manager-aggregation-back.php
+++ CPSC304Project/pages/manager-aggregation-back.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+    session_start();
+    $connection = mysql_connect("localhost", "root", "") or die("<p>Couldn't connect to the database!</p>");
+    mysql_select_db("amusement_park", $connection) or die("<p>Couldn't connect to the database!</p>");
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -46,6 +48,52 @@ session_start();
         <div class="col-md-6 col-md-offset-3">
             <form role="form" id="form-background">
                 <p id="p-label">Aggregation Query Result</p>
+                <?php
+                    if($_POST['aggegrationChoice'] == "0"){
+                        echo "<p id='p-label'>Names of the guests who have the highest account balance:</p>";
+                    }
+                    else{
+                        echo "<p id='p-label'>The sum of all guest account balances:</p>";
+                    }
+                ?>
+                <br />
+                <table class="table table-striped" style="background-color:white">
+                    <thead>
+                        <tr>
+                            <?php
+                            if($_POST['aggegrationChoice'] == "0"){
+                                echo "<th>Name</th>";
+                            }
+                            else{
+                                echo "<th>Sum ($)</th>";
+                            }
+                            ?>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Formulate query
+                        if($_POST['aggegrationChoice'] == "0")
+                            $query = sprintf("SELECT name AS result FROM guest WHERE accountBalance >= ALL (SELECT accountBalance FROM guest);");
+                        else
+                            $query = sprintf("SELECT SUM(accountBalance) AS result FROM guest;");
+
+                        // Perform Query
+                        $result = mysql_query($query);
+
+                        // Check result
+                        // This shows the actual query sent to MySQL, and the error. Useful for debugging.
+                        if (!$result) {
+                            $message  = 'Invalid query: ' . mysql_error() . "\n";
+                            $message .= 'Whole query: ' . $query;
+                            die($message);
+                        }
+
+                        while($row = mysql_fetch_assoc($result))
+                            echo "<tr><td>".$row['result']."</td></tr>";
+                        ?>
+                    </tbody>
+                </table>
                 <a class="btn btn-default" href="manager-account.php">Account Home</a>
             </form>
         </div>
--- CPSC304Project/pages/manager-aggregation-front.php
+++ CPSC304Project/pages/manager-aggregation-front.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+    session_start();
+    $connection = mysql_connect("localhost", "root", "") or die("<p>Couldn't connect to the database!</p>");
+    mysql_select_db("amusement_park", $connection) or die("<p>Couldn't connect to the database!</p>");
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -46,6 +48,17 @@ session_start();
         <div class="col-md-6 col-md-offset-3">
             <form role="form" action="manager-aggregation-back.php" id="form-background" method="post">
                 <p id="p-label">Aggregation Query</p>
+                <div class="radio" id="gender-radio">
+                    <label>
+                        <input type="radio" name="aggegrationChoice" value="0" required />
+                        Find the names of the guests who have the highest account balance.
+                    </label>
+                    <br />
+                    <label>
+                        <input type="radio" name="aggegrationChoice" value="1" />
+                        Find the sum of all guest account balances.
+                    </label>
+                </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
                 <a class="btn btn-default" href="manager-account.php">Cancel</a>
             </form>
